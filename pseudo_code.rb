fb20b41f2cfa2872e04b0fb50d813d9c9652554e
Reni <rgabrielsoto@gmail.com>
2016-07-23 06:42:37 -0400

    pseudo code exercise

---
+++ pseudo_code.rb
@@ -0,0 +1,51 @@
+# casual a method that returns the sum of two integers
+define addition (number)
+get two numbers from user 
+add both numbers 
+return number 
+
+
+#formal method that returns two integers when added
+START 
+SET saved_number (n, x)
+GET n 
+Get x
+PRINT n + x 
+END 
+
+
+# casual a method that takes an array of strings, and returns a string that is all those strings concatenated together
+create array variable that contains a list of strings 
+concatenate array 
+
+#formal a method that takes an array of strings, and returns a string that is all those strings concatenated together
+START
+SET str_arr = ["string1", "string 2", "string 3"]
+str_arr.conc
+END 
+
+# casual a method that takes an array of integers, and returns a new array with every other element
+
+def arr 
+  arr.gets.chomp 
+  arr.every other 
+end 
+
+# formal a method that takes an array of integers, and returns a new array with every other element
+START
+SET arr 
+GET arr 
+arr.every other 
+p arr 
+
+
+
+keyword meaning
+START start of the program
+SET sets a variable we can use for later
+GET retrieve input from user
+PRINT displays output to user
+READ  retrieve value from variable
+IF / ELSE IF / ELSE show conditional branches in logic
+WHILE show looping logic
+END end of the program
