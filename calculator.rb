25cf293f60fc2b921e7cb6c95bc2e71aa186b3d0
Reni <rgabrielsoto@gmail.com>
2016-07-27 05:48:09 -0500

    rubo cop and calculator refactoring

--- calculator.rb
+++ calculator.rb
@@ -6,25 +6,31 @@
 #answer = Kernel.gets()
 #Kernel.puts (answer)
 
-Kernel.puts("Welcome to the calculator!")
-
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide ")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-elsif operator == '4'
-  result = number1.to_f / number2.to_f     
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end 
 
-Kernel.puts("The result is #{result}")
+prompt("Welcome to the calculator!")
+
+prompt("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+prompt("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide ")
+operator = Kernel.gets().chomp()
+
+result = case operator 
+         when '1'
+          number1.to_i() + number2.to_i()
+         when '2'
+          number1.to_i() - number2.to_i()
+         when '3'
+          number1.to_i() * number2.to_i()
+         when '4'
+          number1.to_f / number2.to_f  
+        end 
+
+prompt("The resilt is #{result}")
+
---
+++ hello.rb
@@ -0,0 +1,1 @@
+kernel.puts("hello world")
