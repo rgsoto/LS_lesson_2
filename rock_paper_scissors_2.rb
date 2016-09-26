5e60a7ba6507b271527605ce724847cbff3a36e2
Reni <rgabrielsoto@gmail.com>
2016-09-25 20:06:01 -0500

    bonus features for RPSLS

--- rock_paper_scissors_2.rb
+++ rock_paper_scissors_2.rb
@@ -1,16 +1,24 @@
-VALID_CHOICES = %w(rock paper scissors) # this is a constant
+#VALID_CHOICES = %w(rock paper scissors lizzard spock)
+VALID_CHOICES = %w(r p s l sp)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
-end
+  (first == 'r' && second == 's') ||
+    (first == 'p' && second == 'r') ||
+    (first == 's' && second == 'p') ||
+    (first == 'r' && second == 'l') ||
+    (first == 'l' && second == 'sp') ||
+    (first == 'sp' && second == 's') ||
+    (first == 's' && second == 'l') ||
+    (first == 'p' && second == 'sp') ||
+    (first == 'sp' && second == 'r')
+  end
 
-def display_result(player, computer)
+
+def display_results(player, computer)
   if win?(player, computer)
     prompt("You won!")
   elsif win?(computer, player)
@@ -20,10 +28,20 @@ def display_result(player, computer)
   end
 end
 
+def score(player, computer)
+  if display_results(player) > 5
+    prompt "Here's your score"
+  elsif display_results(computer).count > 5
+    prompt "Computer score"
+  end
+end
+
+
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Choose one: r for rock, p for paper, s for scissors, l for lizzard, and sp for spock!")
+    #prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
 
     if VALID_CHOICES.include?(choice)
@@ -37,7 +55,9 @@ loop do
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
-  display_result(choice, computer_choice)
+
+  display_results(choice, computer_choice)
+  #score(choice, computer_choice)
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
